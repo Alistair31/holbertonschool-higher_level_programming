640228e628f4dbaae38e08a9ad0bf53af2acf1b4
Alistair31 <gabikill@hotmail.fr>
2026-01-13 14:36:40 +0100

    fix: missing sign for negative number + absolute value

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-ld = (number % 10)
+ld = abs(number) % 10
+if number < 0:
+    ld = -ld
 if ld > 5:
     print(f"Last digit of {number} is {ld} and is greater than 5")
 elif ld < 6 and (number % 10) != 0:
-    if number < 0:
-        ld = (-number % 10)
-        print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
-    else:
-        print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
+
+    print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
 else:
     print(f"Last digit of {number} is {ld} and is  0")
