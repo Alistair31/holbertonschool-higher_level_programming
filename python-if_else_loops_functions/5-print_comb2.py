34ab74d3eea22b13396ee0a2ca6c69836a072b2e
Alistair31 <gabikill@hotmail.fr>
2026-01-14 11:28:16 +0100

    fix: unwanted repetition of last elements of range

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for c in range(0, 100):
-    if c == 99:
-        print("{0}".format(c))
+for c in range(0, 99):
     print("{0:02}, ".format(c), end="")
+    if c == 98:
+        print("99".format(c))
