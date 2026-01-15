b9d139e8ebcf3f22b9d64d79cdcbceacba6727bd
Alistair31 <gabikill@hotmail.fr>
2026-01-15 15:21:54 +0100

    fix: print message corrected

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    print("{} arguments.".format(len(argv) - 1))
+    if len(argv) == 2:
+        print("{} argument.".format(len(argv) - 1))
+    else:
+        print("{} arguments.".format(len(argv) - 1))
     for i in range(1, len(argv)):
         if (len(argv) == 1) != "":
             print("{}: {}".format(i, argv[i]))
