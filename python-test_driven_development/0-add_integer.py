588be6c971b866550a32409319c2a710ca7ce81f
Alistair31 <gabikill@hotmail.fr>
2026-01-23 09:19:54 +0100

    fix: doctest and condition for infinite float corrected

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
+'''
+Docstring for 0-add_integer
+'''
+
+
 def add_integer(a, b=98):
-    """
+    '''
     Docstring for add_integer
-    :param a: Description
-    :param b: Description
-    """
+    :param a: first integer to put in add function
+    :param b: second integer to put in add function
+    '''
+    if a == float('inf') or a == float('-inf'):
+        raise ValueError("a must be an integer")
     if isinstance(a, (int, float)) is False:
-        if isinstance(a, float) is True:
-            if a == float('inf') or a == float('-inf'):
-                raise ValueError("a must be an integer")
         raise TypeError("a must be an integer")
+    if b == float('inf') or b == float('-inf'):
+        raise ValueError("b must be an integer")
     if isinstance(b, (int, float)) is False:
-        if isinstance(b, float) is True:
-            if b == float('inf') or b == float('-inf'):
-                raise ValueError("b must be an integer")
         raise TypeError("b must be an integer")
     return int(a) + int(b)
