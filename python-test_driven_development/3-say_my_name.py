ee8be25dd49b79c411ebf9ed76a53f344cad8cb6
Alistair31 <gabikill@hotmail.fr>
2026-01-23 13:52:56 +0100

    fix: missing space after first name, for no last name case

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -15,6 +15,6 @@ def say_my_name(first_name, last_name=""):
     if isinstance(last_name, str) is False:
         raise TypeError("last_name must be a string")
     if last_name == "":
-        print("My name is {}".format(first_name))
+        print("My name is {} ".format(first_name))
     else:
         print("My name is {} {}".format(first_name, last_name))
