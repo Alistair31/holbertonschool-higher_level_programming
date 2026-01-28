92dfe5c57e8d1eeffe9854195eea567843fe0733
Alistair31 <gabikill@hotmail.fr>
2026-01-28 14:04:10 +0100

    fix: add type and value check for initialisation

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -17,6 +17,14 @@ class Rectangle:
         :param width: width of the rectangle
         :param height: heigth of the rectangle
         """
+        if isinstance(width, int) is False:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if isinstance(height, int) is False:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
