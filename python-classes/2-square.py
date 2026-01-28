908effcbaedf5c70eb5111eaa3a992e2bdd8145d
Alistair31 <gabikill@hotmail.fr>
2026-01-28 09:53:52 +0100

    feat: define a square with a size only in integer

---
+++ python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+python-classes.0-square
+"""
+
+
+class Square:
+    """
+    Declare a class named Square
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialise the parameter for the class
+
+        :param self: recall of itself
+        :param size: size of the square
+        """
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
