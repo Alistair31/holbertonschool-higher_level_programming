04a282e4edb647dbb6f07e6b681396b2a8354101
Alistair31 <gabikill@hotmail.fr>
2026-01-28 09:47:41 +0100

    feat: defines a square with a side attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+python-classes.0-square
+"""
+
+
+class Square:
+    """
+    Declare a class named Square
+    """
+
+    def __init__(self, size):
+        """
+        Initialise the parameter for the class
+
+        :param self: recall of itself
+        :param size: size of the square
+        """
+        self.__size = size
