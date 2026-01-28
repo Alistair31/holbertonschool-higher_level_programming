15626bb71a26bc58896e1347475d768556356cd9
Alistair31 <gabikill@hotmail.fr>
2026-01-28 10:00:21 +0100

    feat: define a square and calculate his area

---
+++ python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+python-classes.0-square
+"""
+
+
+class Square:
+    """
+    Declare a class named Square
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialise the parameter for the class
+
+        :param self: recall of itself
+        :param size: size of the square
+        """
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Area of the square
+
+        :param self: recall of itself (Square)
+        """
+        return (self.__size * self.__size)
