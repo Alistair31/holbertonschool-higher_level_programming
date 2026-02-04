29b8208107cae5ba4aa5fd687ae813eeca03de0f
Alistair31 <gabikill@hotmail.fr>
2026-02-04 15:29:27 +0100

    fix: add every other class to the same file because checker want it

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,8 +1,55 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 """
 Create a class Square that inherits from Rectangle.
 """
+
+
+class BaseGeometry:
+    """
+    Class for basic geometry
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Method for checking if the argument is an positive integer
+
+        :param self: recall of the class
+        :param name: name of the parameter
+        :param value: value of the integer
+        """
+
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+    class that define a rectangle
+    """
+
+    def __init__(self, width, height):
+        """
+        initialisation of the class
+
+        :param self: recall of itself
+        :param width: width of the rectangle
+        :param height: height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
 
 
 class Square(Rectangle):
