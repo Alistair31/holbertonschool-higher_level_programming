2a3a3939c671dc22b410d2fadf01683d18d80514
Alistair31 <gabikill@hotmail.fr>
2026-02-04 12:57:23 +0100

    feat: Add an empty area method to the base geometry class

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+5-base_geometry: 1st step of creating a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Class for basic geometry
+    """
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
