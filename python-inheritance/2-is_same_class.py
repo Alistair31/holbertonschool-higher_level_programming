f78157ce21e937c23ee5de12b63473490714d182
Alistair31 <gabikill@hotmail.fr>
2026-02-04 12:46:10 +0100

    feat: function that returns True if the object is in the class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Check if an object is in a class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Method for return true if the object is in the class
+
+    :param obj: object to check
+    :param a_class: class to check
+    """
+
+    if type(obj) is a_class:
+        return True
