bb259e7800ec4271432a759cd32f6dbe2ec55254
Alistair31 <gabikill@hotmail.fr>
2026-02-04 12:51:41 +0100

    feat: function check if the object is an instance of a class that inherited class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+check inheritance
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Method for return if an object inheritance
+
+    :param obj: object to check
+    :param a_class: class to check
+    """
+
+    if obj is a_class:
+        return True
