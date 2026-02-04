33b7f3b782ad13c6555c2ce247ba6b9ad712df92
Alistair31 <gabikill@hotmail.fr>
2026-02-04 12:55:59 +0100

    feat: file that contain an empty class

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+5-base_geometry: 1st step of creating a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Class for basic geometry
+    """
+
+    def __init__(self):
+        pass
