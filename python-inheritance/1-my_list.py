94fcdeceeaf68b87ad5385aa87edf6c21c7e72d6
Alistair31 <gabikill@hotmail.fr>
2026-02-04 11:46:29 +0100

    fix: docstring error

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,12 +6,12 @@ Python-inheritance.1-my_list
 
 class MyList(list):
     """
-    Docstring for Mylist
+    Class that define a list
     """
 
     def print_sorted(self):
         """
-        Method ofr print a sorted list
+        Method of print a sorted list
 
         :param self: Recall itself (MyList)
         """
