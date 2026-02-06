92d619eba4cb815e4eef2162bebd23218f1b140f
Alistair31 <gabikill@hotmail.fr>
2026-02-06 16:26:21 +0100

    fix: correct print message implemented

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -27,11 +27,5 @@ class Dragon(SwimMixin, FlyMixin):
     Docstring for FlyingFish
     """
 
-    def swim(self):
-        print("The flying fish is swimming!")
-
-    def fly(self):
-        print("The flying fish is soaring!")
-
     def roar(self):
         print("The dragon roars!")
