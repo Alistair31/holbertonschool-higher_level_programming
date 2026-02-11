468507371136fb3f01f508698b15b47fe320aa66
Alistair31 <gabikill@hotmail.fr>
2026-02-11 10:17:47 +0100

    fix, docstring for function missing

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,11 @@ python-input_output.0-read_file
 
 
 def read_file(filename=""):
+    """
+    read_file
+
+    :param filename: File to read
+    """
     with open(filename, "r") as f:
         print(f.read())
         f.close()
