39dd9d2124d2bb183fb85a0c23ac4b076d18c5cf
Alistair31 <gabikill@hotmail.fr>
2026-03-04 14:04:27 +0100

    fix: wrong column name in SQL query

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- count the number of TV shows for each genre and list the genres along with their corresponding show counts.
 -- The result should be ordered by the number of shows in descending order.
-SELECT tv_genres.name, COUNT(tv_shows.id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_shows.id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
