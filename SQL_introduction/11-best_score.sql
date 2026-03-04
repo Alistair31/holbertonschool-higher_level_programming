10d5d29c741bd20a2bc3d2227345350babb172a9
Alistair31 <gabikill@hotmail.fr>
2026-03-04 10:02:38 +0100

    fix: correct to order results by score in descending order

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,5 @@
 -- select the score and name of all records in the table `second_table` where the score is higher than or equal to 10
 SELECT score, name
 FROM second_table
-WHERE score >= 10;
+WHERE score >= 10 
+ORDER BY score DESC;
