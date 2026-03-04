bc9e9c23de9434e6015391dd2a066cbfebfdf3e3
Alistair31 <gabikill@hotmail.fr>
2026-03-04 13:25:18 +0100

    feat: list all the TV shows along with their corresponding genre IDs from dumped database

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Write a SQL query to list all the TV shows along with their corresponding genre IDs.
+-- Use a JOIN operation to combine the `tv_shows` and `tv_show_genres` tables based on the foreign key relationship between them.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id;
