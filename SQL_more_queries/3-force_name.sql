76f07243692cad1a6a15ba9e00dfab4c2f5ee4ca
Alistair31 <gabikill@hotmail.fr>
2026-03-04 11:16:41 +0100

    feat: create force_name table with NOT NULL constraint on name column

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- Create a table named `force_name` with the following columns:
+-- - `id`: an integer
+-- - `name`: a string with a maximum length of 256 characters
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
