2876daa525175f5457f7243cfe3a34bbed588f06
Alistair31 <gabikill@hotmail.fr>
2026-03-04 13:04:14 +0100

    fix: select statement correction, change all to specific columns

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Write a SQL query to list all the cities of California using the `cities` and `states` tables. The `cities` table has a foreign key `state_id` that references the `id` column in the `states` table. Use a subquery to find the `id` of California in the `states` table and then use that `id` to find the corresponding cities in the `cities` table.
-SELECT * 
+SELECT id, name
 FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California');
