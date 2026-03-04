845809a30378709d8c006a504d6ec888a210fe2f
Alistair31 <gabikill@hotmail.fr>
2026-03-04 10:59:32 +0100

    fix: wrong database name

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Create a new database and a new user in MySQL and grant them SELECT privileges on a specific database
-CREATE DATABASE IF NOT EXISTS hbtn_0c_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0c_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
