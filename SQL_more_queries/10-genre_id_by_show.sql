7029e79451d9d8bccc64388a507b47bb0c7113f7
Alistair31 <gabikill@hotmail.fr>
2026-03-04 13:28:42 +0100

    fix: correct output order of SQL query results

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,5 @@
 -- Use a JOIN operation to combine the `tv_shows` and `tv_show_genres` tables based on the foreign key relationship between them.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title;
