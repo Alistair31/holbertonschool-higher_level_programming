70637a74c0effc2c43a88c11763677ed9cbec097
Alistair31 <gabikill@hotmail.fr>
2026-03-06 15:26:38 +0100

    feat: first state model, with id and name attributes

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines the State class which maps
+to the 'states' table in the database.
+It uses SQLAlchemy's declarative base to create
+the mapping between the class and the table.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class State(Base):
+    """State class that maps to the 'states' table in the database."""
+    __tablename__ = 'states'
+    id = Column("states_id", Integer, primary_key=True)
+    name = Column("states_name", String(128), nullable=False)
