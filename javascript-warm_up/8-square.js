a7589e706f386d6f3cfd5c5a75acad1d0317bffa
Alistair31 <gabikill@hotmail.fr>
2026-03-19 19:54:06 +0100

    feat: script that prints a square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const args = process.argv;
+const x = parseInt(args[2]);
+let i;
+if (isNaN(x)) {
+  console.log('Missing size');
+}
+for (i = 0; i < x; i++) {
+  console.log('X'.repeat(x));
+}
