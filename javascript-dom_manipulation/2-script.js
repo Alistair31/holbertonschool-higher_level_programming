de79283f7ca5426a3427bcb488fb8c9dc45c4dfc
Alistair31 <gabikill@hotmail.fr>
2026-03-30 10:00:29 +0200

    fix: update header color toggle functionality and improve event listener for red header

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
-const header = document.getElementById("red_header");
-header.classList.add("red");
+const redHeader = document.getElementById("red_header");
+redHeader.addEventListener("click", function () {
+  document.querySelector("header").classList.add("red");
+});
--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const header = document.querySelectorAll("header");
 const toggle_header = document.getElementById("toggle_header");
-toggle_header.addEventListener("click", function() {
-    if (header[0].style.color === "rgb(255, 0, 0)") {
-        header[0].style.color = "#00FF00";
-    } else {
-        header[0].style.color = "#ff0000";
-    }
+toggle_header.addEventListener("click", function () {
+	if (header.classList === "green") {
+		document.querySelector("header").classList.replace("red")
+	} else if (header.classList === "red") {
+		document.querySelector("header").classList.replace("green")
+	}
 });
